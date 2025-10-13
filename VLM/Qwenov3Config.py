cdbfa959811f4d9b6c2b91e4a07ee69efa9e3f07
天烨 <83327454+Tian-ye1214@users.noreply.github.com>
2025-10-13 23:17:16 +0800

    添加Processor

--- VLM/Qwenov3Config.py
+++ VLM/Qwenov3Config.py
@@ -1,9 +1,14 @@
-from transformers import PreTrainedModel, PretrainedConfig, GenerationMixin
+from typing import Optional, Union
+from transformers import PreTrainedModel, PretrainedConfig, GenerationMixin, Cache, BatchFeature
 from modelscope import AutoConfig, AutoProcessor, AutoModel, AutoTokenizer, AutoModelForCausalLM
 import torch
 import torch.nn as nn
+from transformers.image_utils import ImageInput
 from transformers.modeling_outputs import CausalLMOutputWithPast
 from liger_kernel.transformers import LigerCrossEntropyLoss
+from transformers.processing_utils import Unpack, ProcessorMixin
+from transformers.tokenization_utils_base import TextInput, PreTokenizedInput
+from transformers.utils import TransformersKwargs
 
 
 class Qwenov3Config(PretrainedConfig):
@@ -33,6 +38,45 @@ class Qwenov3Config(PretrainedConfig):
         self.vocab_size = vocab_size
         
         super().__init__(**kwargs)
+
+
+class Qwenov3Processor(ProcessorMixin):
+    attributes = ["image_processor", "tokenizer"]
+    image_processor_class = "AutoImageProcessor"
+    tokenizer_class = "AutoTokenizer"
+
+    def __init__(self, image_processor=None, tokenizer=None, chat_template=None, image_pad_num=49, **kwargs):
+        self.image_token = "<|image_pad|>"
+        self.image_pad_num = image_pad_num
+        if chat_template is None and tokenizer is not None:
+            chat_template = getattr(tokenizer, "chat_template", None)
+        super().__init__(image_processor, tokenizer, chat_template=chat_template)
+
+    def __call__(
+        self,
+        images: Optional[ImageInput] = None,
+        text: Union[TextInput, PreTokenizedInput, list[TextInput], list[PreTokenizedInput]] = None,
+        return_tensors: str = "pt",
+        **kwargs,
+    ) -> BatchFeature:
+        image_inputs = {}
+        if images is not None:
+            image_inputs = {'pixel_values': self.image_processor(images=images, return_tensors="pt")['pixel_values']}
+
+        if not isinstance(text, list):
+            text = [text]
+
+        processed_text = []
+        for t in text:
+            replacement = '<|vision_start|>' + '<|image_pad|>' * self.image_pad_num + '<|vision_end|>'
+            if '<image>' not in t:
+                t = t.replace('<|im_end|>', '<image><|im_end|>', 1)
+            processed_text.append(t.replace('<image>', replacement))
+
+        tokenizer_kwargs = {k: v for k, v in kwargs.items() if k not in ['images']}
+        text_inputs = self.tokenizer(processed_text, return_tensors=return_tensors, **tokenizer_kwargs)
+
+        return BatchFeature(data={**text_inputs, **image_inputs})
 
 
 class Qwenov3(GenerationMixin, PreTrainedModel):
@@ -99,46 +143,61 @@ class Qwenov3(GenerationMixin, PreTrainedModel):
             for param in self.llm_model.parameters():
                 param.requires_grad = False
 
-    def forward(self, input_ids=None, labels=None, pixel_values=None, attention_mask=None, 
-                inputs_embeds=None, past_key_values=None, use_cache=None, **kwargs):
-
-        if inputs_embeds is None:
-            text_embeds = self.llm_model.get_input_embeddings()(input_ids)
-            if pixel_values is not None:
-                image_embeds = self.vision_model(pixel_values).last_hidden_state
-                patch_embeds = image_embeds[:, 5:, :]  # [batch, 196, 1024]
-                b, num_patches, hidden_dim = patch_embeds.shape
-                patch_embeds = patch_embeds.view(b, num_patches // 4, hidden_dim * 4)  # [batch, 49, 4096]
-                image_features = self.adapter(patch_embeds)
-                text_embeds = text_embeds.to(image_features.dtype)
-                inputs_embeds = self.merge_input_ids_with_image_features(image_features, text_embeds, input_ids)
-            else:
-                inputs_embeds = text_embeds
+    def forward(
+        self,
+        input_ids: Optional[torch.LongTensor] = None,
+        pixel_values: Optional[torch.LongTensor] = None,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        past_key_values: Optional[Cache] = None,
+        labels: Optional[torch.LongTensor] = None,
+        use_cache: Optional[bool] = None,
+        cache_position: Optional[torch.LongTensor] = None,
+        logits_to_keep: Union[int, torch.Tensor] = 0,
+        **kwargs: Unpack[TransformersKwargs],
+    ):
+        text_embeds = self.llm_model.get_input_embeddings()(input_ids)
+        if pixel_values is not None:
+            image_embeds = self.vision_model(pixel_values).last_hidden_state
+            patch_embeds = image_embeds[:, 5:, :]  # [batch, 196, 1024]
+            b, num_patches, hidden_dim = patch_embeds.shape
+            patch_embeds = patch_embeds.view(b, num_patches // 4, hidden_dim * 4)  # [batch, 49, 4096]
+            image_features = self.adapter(patch_embeds)
+            text_embeds = text_embeds.to(image_features.dtype)
+            inputs_embeds = self.merge_input_ids_with_image_features(image_features, text_embeds, input_ids)
+        else:
+            inputs_embeds = text_embeds
 
         outputs = self.llm_model(
-            inputs_embeds=inputs_embeds, 
+            input_ids=input_ids,
             attention_mask=attention_mask,
+            position_ids=position_ids,
             past_key_values=past_key_values,
+            inputs_embeds=inputs_embeds,
             use_cache=use_cache,
-            return_dict=True
+            cache_position=cache_position,
+            **kwargs,
         )
-        
-        logits = outputs.logits
+
+        hidden_states = outputs.last_hidden_state
+        slice_indices = slice(-logits_to_keep, None) if isinstance(logits_to_keep, int) else logits_to_keep
+        logits = self.lm_head(hidden_states[:, slice_indices, :])
+
         loss = None
         if labels is not None:
             loss_fct = LigerCrossEntropyLoss(ignore_index=self.tokenizer.pad_token_id)
             loss = loss_fct(logits.view(-1, logits.size(-1)), labels.view(-1).to(logits.device))
-        
+
         return CausalLMOutputWithPast(
-            loss=loss, 
+            loss=loss,
             logits=logits,
             past_key_values=outputs.past_key_values,
             hidden_states=outputs.hidden_states,
-            attentions=outputs.attentions
+            attentions=outputs.attentions,
         )
 
     @torch.inference_mode()
-    def generate(self, input_ids=None, pixel_values=None, attention_mask=None, 
+    def generate(self, input_ids=None, pixel_values=None, attention_mask=None,
                  max_new_tokens=512, temperature=0.7, top_p=0.8, top_k=20,
                  do_sample=True, num_beams=1, use_cache=True, **kwargs):
         if pixel_values is not None:
